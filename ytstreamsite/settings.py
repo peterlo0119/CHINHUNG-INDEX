426840006822240f1bfaf901c0489639837b1310
peterlo0119 <peterlo0119@gmail.com>
2025-06-23 15:58:50 +0800

    enable static files with whitenoise

--- ytstreamsite/settings.py
+++ ytstreamsite/settings.py
@@ -18,6 +18,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -61,3 +62,5 @@ USE_TZ = False
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'mainapp/static')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
